2fb087659e94703bd28ce352b9f985971f941f11
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-07-18 18:13:23 +0530

    database created

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ import express from 'express' ;
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import 'dotenv/config'
+import connectDB from './config/database.js';
 
 const app = express();
 const Port = process.env.PORT || 8000
+
+connectDB();
 
 app.use(express.json()) ;
 app.use(cookieParser());
