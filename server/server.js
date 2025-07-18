ea4500d1098e7001ea6c8d756e9881fd98a251df
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-07-18 17:55:32 +0530

    server created

---
+++ server/server.js
@@ -0,0 +1,19 @@
+
+import express from 'express' ;
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+import 'dotenv/config'
+
+const app = express();
+const Port = process.env.PORT || 8000
+
+app.use(express.json()) ;
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+
+app.get('/', (req,res) => {
+  res.send("hii from server")
+})
+
+app.listen(Port, () => console.log("server started"));
