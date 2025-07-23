7061bba10070919ec33e01eb1b79b26281e54649
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-07-23 19:06:24 +0530

    make changes in  paths

--- aneesh-portfolio/src/App.jsx
+++ aneesh-portfolio/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Route , Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import VerifyEmail from './pages/VerifyEmail'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
 
 function App() {
   return (
-    <>
-    <Home />
-    </>
+      <div>
+        <Routes>
+          <Route  path='/'  element={<Home />}  />
+          <Route  path='/verify-email'  element={<VerifyEmail />}  />
+          <Route  path='/reset-password'  element={<ResetPassword />}  />
+          <Route  path='/login'  element={<Login />}  />
+        </Routes>
+      </div>
   )
 }
 
