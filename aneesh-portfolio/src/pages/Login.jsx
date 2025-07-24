e9bffa371c5b2f5236607d701350f41b2f4a81d0
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-07-24 15:42:39 +0530

    create account page created

--- aneesh-portfolio/src/pages/Login.jsx
+++ aneesh-portfolio/src/pages/Login.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+   const[state,setState] = useState('Sign Up');
+
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-white to-gray-500'>
+      
+
+      <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
+         <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? "Create Account" : "Login"}</h2>
+         <p className=' text-sm text-center mb-6'>{state === 'Sign Up' ? "Create your account " : "Login to your account" }</p>
+
+         <form>
+
+          {state === 'Sign Up' && (<div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+            <img src=''></img>
+            <input type='text' placeholder='Full Name' required  className='bg-transparent outline-none' />
+          </div>)}
+          
+
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C] '>
+            <img src=''></img>
+            <input type='email' placeholder='Email Id' required className='bg-transparent outline-none' />
+          </div>
+
+           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C] '>
+            <img src=''></img>
+            <input type='password' placeholder='Password' required className='bg-transparent outline-none' />
+          </div>
+
+          <p className='mb-4  text-indigo-500 cursor-pointer'>Forgot Password</p> 
+
+          <button     className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-semibold cursor-pointer ' >{state}</button>
+
+         </form>
+
+         {state === 'Sign Up' ? (<p className='mt-4  text-white  text-center text-xs'>Already have an account? 
+          <span className='mt-4 ml-2  text-white cursor-pointer text-xs  hover:text-blue-400  ' >Login here</span>
+         </p>) : (<p className='mt-4  text-white cursor-pointer text-center text-xs'>Don't have an account? 
+          <span className='mt-4 ml-2  text-white cursor-pointer text-xs  hover:text-blue-400  ' >Sign Up</span>
+         </p>
+)}
+
+          
+      </div>
+
+    </div>
+
+
   )
 }
 
