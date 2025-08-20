a8070e7fe4d9b59188ee31ec1eff162424bfd40f
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-08-20 23:26:45 +0530

    Dashboard (comp) change

--- Dashboard/my-app/src/pages/Home.jsx
+++ Dashboard/my-app/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const AdminDashboard = () => {
   return (
     <div className="min-h-screen bg-gradient-to-r from-gray-900 to-black p-6 sm:p-6">
       <h1 className="text-2xl sm:text-3xl font-bold text-white mb-6 text-center">
-        📩 Dashboard
+        📩 Portfolio Dashboard 
       </h1>
 
       {loading ? (
